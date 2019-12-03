e932ff58259a80e3f798752833d638fd0593b447
Michal Kolář <michal.kolar81@gmail.com>
2019-12-03 23:47:36 +0100

    Updates

---
+++ precache-manifest.6c76e43a3fd7754f0c05d48292e459d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd76610be1a8cb8219458efb2f255c7b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "30093ebd4bde9a5699fe",
+    "url": "/monsters-rolodex/static/css/main.c3bf40b2.chunk.css"
+  },
+  {
+    "revision": "6ef49256656a9eb2de46",
+    "url": "/monsters-rolodex/static/js/2.8629efad.chunk.js"
+  },
+  {
+    "revision": "30093ebd4bde9a5699fe",
+    "url": "/monsters-rolodex/static/js/main.4c46ceb0.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
